b5236e0779bf386619fa4ed5c76c5572de49d7e0
eric soucy <eric.soucy@gmail.com>
2020-05-08 18:46:11 -0400

    colors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styles: [
+    `
+      .online {
+        color: white;
+      }
+    `,
+  ],
 })
 export class AppComponent {
+  display = false;
+  clicks = [];
+
+  buttonClicked() {
+    this.display = !this.display;
+    this.clicks.push("clicked: " + Date.now());
+  }
+
+  getColor() {
+    return this.clicks.length > 4 ? "blue" : "none";
+  }
 }
